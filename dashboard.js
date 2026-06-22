222db9e2103613a997a287061ebc305847ada58a
Ariyouris <ayigh77@gmail.com>
2026-06-23 05:56:49 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,33 +1,37 @@
 const user = JSON.parse(localStorage.getItem("s4s_user"));
 
 if (!user) {
+
     window.location.href = "./index.html";
+
 }
 
-document.getElementById("welcome").innerHTML = `
-<p style="text-align:center;margin-bottom:30px;">
-Welcome, <b>${user.name}</b>
-</p>
-`;
+document.getElementById("welcome").innerHTML =
+`WELCOME, ${user.name.toUpperCase()}`;
+
 
 document.getElementById("indie").onclick = () => {
+
     window.open(
-        "https://open.spotify.com/playlist/ISI_LINK_INDIE",
+        "LINK_PLAYLIST_INDIE",
         "_blank"
     );
+
 };
 
 document.getElementById("prime").onclick = () => {
+
     window.open(
-        "https://open.spotify.com/playlist/ISI_LINK_PRIMEZONE",
+        "LINK_PLAYLIST_PRIMEZONE",
         "_blank"
     );
+
 };
 
 document.getElementById("logout").onclick = () => {
 
     localStorage.removeItem("s4s_user");
 
-    location.href = "./index.html";
+    window.location.href="./index.html";
 
 };
