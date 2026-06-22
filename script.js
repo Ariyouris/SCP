e91c03bfa8f93b9e03910d623d8d2b3739901d11
Ariyouris <ayigh77@gmail.com>
2026-06-23 02:08:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-const CLIENT_ID = "475553127157-nn2o47e7273qrk4rp46aljldoks35bbm.apps.googleusercontent.com";
+const CLIENT_ID =
+"475553127157-nn2o47e7273qrk4rp46aljldoks35bbm.apps.googleusercontent.com";
 
 window.onload = () => {
 
-  // Kalau sudah login, langsung dashboard
-  if (localStorage.getItem("s4s_user")) {
-    window.location.href = "./pages/dashboard.html";
-    return;
-  }
-
   google.accounts.id.initialize({
+
     client_id: CLIENT_ID,
+
     callback: handleCredentialResponse
+
   });
 
 };
 
-document.getElementById("googleLogin").onclick = () => {
-  google.accounts.id.prompt();
-};
+document
+.getElementById("googleLogin")
+.addEventListener("click", () => {
 
-function handleCredentialResponse(response) {
+  google.accounts.id.renderButton(
 
-  const payload = JSON.parse(
-    atob(response.credential.split(".")[1])
+    document.createElement("div"),
+
+    {}
+
   );
 
-  localStorage.setItem("s4s_user", JSON.stringify(payload));
+  google.accounts.oauth2.initTokenClient({
 
-  window.location.href = "./pages/dashboard.html";
+    client_id: CLIENT_ID,
+
+    scope: "openid email profile",
+
+    callback: (response) => {
+
+      console.log(response);
+
+    }
+
+  }).requestAccessToken();
+
+});
+
+function handleCredentialResponse(response){
+
+  console.log(response);
+
 }
